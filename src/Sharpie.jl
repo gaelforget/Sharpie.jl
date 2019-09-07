220e9d76375eb0f1371bbdd06498dd631fb7b817
gaelforget <gforget@mit.edu>
2019-09-07 13:08:09 -0400

    fix file path

--- src/Sharpie.jl
+++ src/Sharpie.jl
@@ -3,8 +3,8 @@ module Sharpie
 using Images, FileIO
 
 function BeforeAndAfter()
-before=load(pathof(Sharpie)*"/../../figs/Sharpie_before.jpg");
-after=load(pathof(Sharpie)*"/../../figs/Sharpie_after.jpg");
+before=load(dirname(pathof(Sharpie))[1:end-3]*"figs/Sharpie_before.jpg");
+after=load(dirname(pathof(Sharpie))[1:end-3]*"figs/Sharpie_after.jpg");
 display([before after])
 end
 
